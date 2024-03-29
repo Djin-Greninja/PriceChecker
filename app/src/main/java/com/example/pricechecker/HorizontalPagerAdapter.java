5559d1f32e8d22a61677ea2368e2c6d608e8770b
Ian <adrian.nebasa15@gmail.com>
2024-03-29 17:43:35 +0800

    some changes

---
+++ app/src/main/java/com/example/pricechecker/HorizontalPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.pricechecker;
+
+import android.content.Context;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.List;
+
+public class HorizontalPagerAdapter extends PagerAdapter {
+
+    private List<String> mData;
+    private Context mContext;
+
+    public HorizontalPagerAdapter(Context context, List<String> data) {
+        mContext = context;
+        mData = data;
+    }
+
+    @Override
+    public int getCount() {
+        return mData.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.view_pager_item, container, false);
+        TextView textView = view.findViewById(R.id.textView);
+        textView.setText(mData.get(position));
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
+
--- app/src/main/java/com/example/pricechecker/LnadingPageHome.java
+++ app/src/main/java/com/example/pricechecker/LnadingPageHome.java
@@ -7,6 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LnadingPageHome extends AppCompatActivity {
 
@@ -15,5 +19,15 @@ public class LnadingPageHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_lnading_page_home);
+
+        ViewPager viewPager = findViewById(R.id.view);
+
+        List<String> data = new ArrayList<>();
+        data.add("Item 1");
+        data.add("Item 2");
+        data.add("Item 3");
+
+        HorizontalPagerAdapter adapter = new HorizontalPagerAdapter(this, data);
+        viewPager.setAdapter(adapter);
     }
 }
