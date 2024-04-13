fbebbd5f5369f8e1dfce092f858d6f0a4e3eb41f
Ian <adrian.nebasa15@gmail.com>
2024-04-13 13:00:37 +0800

    Home navbar

--- app/src/main/java/com/example/pricechecker/DiscoverFragment.java
+++ app/src/main/java/com/example/pricechecker/DiscoverFragment.java
@@ -1,6 +1,12 @@
 package com.example.pricechecker;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.viewmodel.CreationExtras;
 
@@ -9,5 +15,9 @@ public class DiscoverFragment extends Fragment {
     @Override
     public CreationExtras getDefaultViewModelCreationExtras() {
         return super.getDefaultViewModelCreationExtras();
+    }
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_discover, container, false);
     }
 }
--- app/src/main/java/com/example/pricechecker/HomeFragment.java
+++ app/src/main/java/com/example/pricechecker/HomeFragment.java
@@ -1,13 +1,24 @@
 package com.example.pricechecker;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.viewmodel.CreationExtras;
 
 public class HomeFragment extends Fragment {
-    @NonNull
-    @Override
-    public CreationExtras getDefaultViewModelCreationExtras() {
-        return super.getDefaultViewModelCreationExtras();
+        @NonNull
+        @Override
+        public CreationExtras getDefaultViewModelCreationExtras () {
+            return super.getDefaultViewModelCreationExtras();
+
+        }
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 }
