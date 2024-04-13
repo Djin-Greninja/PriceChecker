338d84ce976d76feddc671c1a08d878ee7bbeca1
Ian <adrian.nebasa15@gmail.com>
2024-04-13 17:44:56 +0800

    Home navbar

--- app/src/main/java/com/example/pricechecker/MainActivity.java
+++ app/src/main/java/com/example/pricechecker/MainActivity.java
@@ -7,7 +7,6 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.pricechecker.databinding.ActivityMainBinding;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.bottomNavigationView.setBackgroundResource(0);
         replaceFragment(new HomeFragment());
 
-        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
             if (item.getItemId() == R.id.action_home) {
                 replaceFragment(new HomeFragment());
@@ -49,46 +49,3 @@ public class MainActivity extends AppCompatActivity {
     }
 }
 
-   /**!bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-
-                if (itemId == R.id.action_home) {
-                    // Handle Home item click
-                    // For example, navigate to the Home fragment
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container, new HomeFragment())
-                            .commit();
-                    return true;
-                } else if (itemId == R.id.action_discover) {
-                    // Handle Discover item click
-                    // For example, navigate to the Discover fragment
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container, new DiscoverFragment())
-                            .commit();
-                    return true;
-                } else if (itemId == R.id.action_grocery_list) {
-                    // Handle Grocery List item click
-                    // For example, navigate to the Grocery List fragment
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container, new ListFragment())
-                            .commit();
-                    return true;
-                } else if (itemId == R.id.action_profile) {
-                    // Handle Profile item click
-                    // For example, navigate to the Profile fragment
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.container, new ProfileFragment())
-                            .commit();
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
-    }
-}
-**/
