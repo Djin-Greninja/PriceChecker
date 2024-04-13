429d64f62392347ff0c6fc191a8735a416e50eb1
Ian <adrian.nebasa15@gmail.com>
2024-04-13 12:25:42 +0800

    Home navbar

--- app/src/main/java/com/example/pricechecker/MainActivity.java
+++ app/src/main/java/com/example/pricechecker/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.pricechecker;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,12 +25,14 @@ public class MainActivity extends AppCompatActivity {
     private final long PERIOD_MS = 6000; // Time in milliseconds between successive task executions
     private Handler handler = new Handler();
     private Runnable runnable;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         location = findViewById(R.id.button2);
         sV = findViewById(R.id.searchBar);
         viewPager = findViewById(R.id.viewPager1);
@@ -39,6 +47,49 @@ public class MainActivity extends AppCompatActivity {
 
         // Start auto-scrolling
         startAutoScroll();
+        // Set listener for item selection events
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_home:
+                        // Handle Home item click
+                        // For example, navigate to the Home fragment
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, new HomeFragment())
+                                .commit();
+                        return true;
+                    case R.id.action_discover:
+                        // Handle Discover item click
+                        // For example, navigate to the Discover fragment
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, new DiscoverFragment())
+                                .commit();
+                        return true;
+                    case R.id.action_grocery_list:
+                        // Handle Grocery List item click
+                        // For example, navigate to the Grocery List fragment
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, new ListFragment())
+                                .commit();
+                        return true;
+                    case R.id.action_profile:
+                        // Handle Profile item click
+                        // For example, navigate to the Profile fragment
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.container, new ProfileFragment())
+                                .commit();
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
     }
 
     private void startAutoScroll() {
