ef759afa0c5bc4a569997ebf47ca2354efdb1e62
Ian <adrian.nebasa15@gmail.com>
2024-04-18 20:27:14 +0800

    Home navbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("androidx.activity:activity:1.8.0")
     implementation ("com.google.android.material:material:1.4.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
