34d4738868e30ddbb2ad20f910fbb9d9fdc2c63d
Ian <adrian.nebasa15@gmail.com>
2024-04-25 11:10:45 +0800

    Profile Fragment

--- app/src/main/java/com/example/pricechecker/User.java
+++ app/src/main/java/com/example/pricechecker/User.java
@@ -4,12 +4,15 @@ public class User {
     private String username;
     private String email;
     private String password;
-
+    private String fullName;
+    private String gender;
+    private String birthday;
+    private String phoneNumber;
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String fullName, String gender, String birthday, String phoneNumber) {
         this.username = username;
         this.email = email;
         this.password = password;
@@ -25,5 +28,20 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 }
