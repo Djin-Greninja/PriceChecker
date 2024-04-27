3c8ef0cd711ae0ecf28401a9a35a3784980f1e91
Ian <adrian.nebasa15@gmail.com>
2024-04-27 16:23:59 +0800

    Update Account Info

---
+++ app/src/main/java/com/example/pricechecker/Update_profileFragment.java
@@ -0,0 +1,25 @@
+package com.example.pricechecker;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Update_profileFragment extends Fragment {
+
+    @SuppressLint("MissingInflatedId")
+    @NonNull
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_update_profile_page, container, false);
+
+
+        return view;
+    }
+}
